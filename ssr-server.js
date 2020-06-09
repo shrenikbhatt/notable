0a0eee20b533a7626f5b316d045ce57bdcf2ee3a
Shrenik Bhatt <shre1106@gmail.com>
2020-06-08 20:03:26 -0400

    Added variable port for server.

--- ssr-server.js
+++ ssr-server.js
@@ -10,6 +10,8 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const PORT = process.env.PORT || 3000;
+
     
 app.prepare()
 .then(() => {
@@ -27,9 +29,9 @@ app.prepare()
 
   
     
-  server.listen(3000, (err) => {
+  server.listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:'+PORT)
   })
 })
 .catch((ex) => {
